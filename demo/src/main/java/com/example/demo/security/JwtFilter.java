11b5b7e884811bf461889bc9babdcd0b2689df3d
Ridge57 <juniorridge93@yahoo.fr>
2025-06-09 15:33:00 +0200

    remove unused import

--- demo/src/main/java/com/example/demo/security/JwtFilter.java
+++ demo/src/main/java/com/example/demo/security/JwtFilter.java
@@ -1,14 +1,14 @@
 package com.example.demo.security;
 
 import com.example.demo.service.JwtService;
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
