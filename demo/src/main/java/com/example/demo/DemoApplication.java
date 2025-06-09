9aa294299487ca0deccef32b9366c1ed56105568
97535J <ridge.moulengui@externe.maif.fr>
2025-06-09 02:17:42 +0200

    ajout du cache caffeine

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,16 +1,20 @@
 package com.example.demo.service;
 
 import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 public class UserService implements UserDetailsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
 
@@ -18,15 +22,15 @@ public class UserService implements UserDetailsService {
         this.userRepository = userRepository;
     }
     @Override
+    @Cacheable(value = "users",key = "#username")
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("loadUserByUsername");
+        logger.info("loadUserByUsername : recherche du user {}",username);
         com.example.demo.model.User foundUser = userRepository.getUsers()
                 .stream()
                 .filter(user->user.getUsername().equals(username))
                 .findFirst()
                 .orElseThrow(()->new RuntimeException("Le user n'a pas été trouvé"));
-        System.out.println(foundUser);
-
+        logger.info("loadUserByUsername : user {} trouvé !",username);
         return new User(foundUser.getUsername(), foundUser.getPassword(), List.of());
     }
 }
